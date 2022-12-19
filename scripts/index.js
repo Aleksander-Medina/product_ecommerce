4bff3648f3c64fe30e237b674157ca194d1876eb
Aleksander-Medina <aleksmedina23@hotmail.com>
2022-12-19 16:24:42 -0600

    Se agregaron funcionalidades de JavaScript

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+let nombre= "Bienvenido";
+let visita = 0;
+let existe= false;
+let duda= undefined;
+let nada= null;
+let numero1= 4;
+let numero2= 40;
+let edad=0;
+
+alert(nombre + " eres la visita numero " + (visita + 1) );
+
+alert(numero1+numero2);
+
+edad=prompt("Inserte su edad porfavor")
+if(edad>17) { alert("Proceda porfavor")
+}else{
+    alert("Lo siento, no tiene la edad requerida")
+}
+
+console.log= (nombre + visita + existe + duda + nada);
